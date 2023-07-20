58db5fa11f6a29b85c517fc1b8240f28cf885624
Felix Wittwer <fwittwer@lbl.gov>
2023-07-20 15:51:38 -0700

    fix mpi bugs

--- example/Si-PBEsol/Si.py
+++ example/Si-PBEsol/Si.py
@@ -8,14 +8,14 @@ from mpi4py import MPI
 import phono3py
 from phono3py import Phono3py
 
-def run_thermal_conductivity(log_level):
+def run_thermal_conductivity(is_root=False):
     """Run RTA thermal conductivity calculation from input files."""
-    ph3 = phono3py.load("phono3py_disp.yaml", log_level=log_level)
+    ph3 = phono3py.load("phono3py_disp.yaml", log_level=2*is_root)
     ph3.mesh_numbers = [11, 11, 11]
     ph3.init_phph_interaction()
     ph3.run_thermal_conductivity(
         temperatures=range(0, 101, 10), 
-        write_kappa=True,
+        write_kappa=is_root,
         pinv_solver=2,
         is_LBTE=True)
     # Conductivity_RTA object
@@ -38,4 +38,4 @@ if __name__ == "__main__":
     with open(f'log_rank{rank}.txt', 'w') as f:
         with redirect_stdout(f):
             create_supercells_with_displacements()
-            run_thermal_conductivity(log_level = 2*(rank==0))
+            run_thermal_conductivity(rank==0)
